d698bf8c59067c2f52546246ba3379bfd7d28c20
mollo <mollo@PCMOIKAS>
2023-10-02 14:55:00 -0600

    Agregar archivos de proyecto.

---
+++ practica 3 arreglos en Csharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = { 10, 20, 30, 40, 50, 60 };
+        int numeroDeseado = numeros[1];
+
+        Console.WriteLine("el numero deseado es:" + numeroDeseado);
+        Console.ReadLine();
+    }
+}
